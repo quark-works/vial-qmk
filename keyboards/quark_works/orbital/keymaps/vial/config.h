31a18d9d3077840286f95b384809d0a8f4ac7c7a
quark-works <brandon.abr@hotmail.com>
2025-06-21 20:09:44 +0000

    Added initial orbital firmware

---
+++ keyboards/quark_works/orbital/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0B, 0x1D, 0x25, 0x14, 0x6A, 0xA3, 0x39, 0x5B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0}
+#define VIAL_UNLOCK_COMBO_COLS {0}
